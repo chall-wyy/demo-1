7524a4d12455b7add4860fa74790d15db62b2d9d
wanyuanyuan <wanyuanyuan@yxqiche.com>
2019-11-15 14:21:15 +0800

    ehcache

--- demo-1/src/main/java/com/example/service/UserService.java
+++ demo-1/src/main/java/com/example/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.User;
@@ -11,6 +12,7 @@ public class UserService {
 	@Autowired
 	UserMapper userMapper;
 	
+	@Cacheable(cacheNames = {"user"}, key = "targetClass + methodName + #p0")
 	public User Sel(int id) {
 		return userMapper.Sel(id);
 	}
